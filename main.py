1d53bad5e38a979c4568b025b0b95408086c5ba1
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2024-12-24 16:05:41 +0800

    optimizer

--- main.py
+++ main.py
@@ -138,14 +138,59 @@ class Optimizer_SGD():
     def post_update_params(self):
         self.iterations += 1
         
-"""class Optimizer_SGD():
-    def __init__(self, learning_rate = 1.0):
+class Optimizer_Adagrad():
+    def __init__(self, learning_rate=1.0, decay=0., epsilon = 1e-7):
         self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon #so we can never divide by zero
         
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+    
     def update_params(self, layer):
-        layer.weights += -self.learning_rate * layer.dweights
-        layer.biases += -self.learning_rate * layer.dbiases"""
+        if not hasattr(layer, "weight_cache"):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+        
+        #updating cache with squared current gradients
+        layer.weight_cache += layer.dweights**2
+        layer.bias_cache += layer.dbiases**2
+
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+        
+    def post_update_params(self):
+        self.iterations += 1
             
+class Optimizer_RMSprop():
+    def __init__(self, learning_rate=1.0, decay=0., epsilon = 1e-7):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon #so we can never divide by zero
+        
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+    
+    def update_params(self, layer):
+        if not hasattr(layer, "weight_cache"):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+        
+        #updating cache with squared current gradients
+        layer.weight_cache += layer.dweights**2
+        layer.bias_cache += layer.dbiases**2
+
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+        
+    def post_update_params(self):
+        self.iterations += 1
 if __name__ == "__main__":
     nnfs.init()
     X, y = spiral_data(samples=100, classes=3)
@@ -203,7 +248,8 @@ if __name__ == "__main__":
     activation1 = Activation_ReLU()
     dense2 = Layer_Dense(64, 3)
     loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
-    optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
+    #optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9) #max 93.3% acc
+    #optimizer = Optimizer_Adagrad(decay=1e-4) #max 86% acc
     
     for epoch in range(10001):
         dense1.forward(X)
