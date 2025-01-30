8c32ddbded6d21170f11489640e21f9d426a26c9
Kai Chen Chai <65500723+kaichenchai@users.noreply.github.com>
2025-01-30 08:00:17 +0000

    up to page 337, need to add data_loss to training loop

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import nnfs
 from nnfs.datasets import spiral_data
 
 class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons, weight_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l1=0, bias_regularizer_l2=0):
         """Note that we’re initializing weights
         to be (inputs, neurons), rather than ( neurons, inputs) . We’re doing this ahead instead of
         transposing every time we perform a forward pass, as explained in the previous chapter."""
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.weight_regularizer_l1=weight_regularizer_l1
+        self.weight_regularizer_l2=weight_regularizer_l2
+        self.bias_regularizer_l1=bias_regularizer_l1
+        self.bias_regularizer_l2=bias_regularizer_l2
         
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
@@ -48,6 +52,27 @@ class Activation_Softmax:
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
             
 class Loss:
+    def regularization_loss(self, layer):
+        regularization_loss = 0
+
+        #l1 weights, when factor is greater than 0
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+
+        #l2 weights
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(layer.weights * layer.weights)
+
+        #l1 biases
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+        
+        #l2 biases
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(layer.biases * layer.biases)
+
+        return regularization_loss
+
     def calculate(self, output, y):
         sample_losses = self.forward(output, y)
         data_loss = np.mean(sample_losses)
@@ -302,6 +327,7 @@ if __name__ == "__main__":
         dense1.forward(X)
         activation1.forward(dense1.output)
         dense2.forward(activation1.output)
+
         loss = loss_activation.forward(dense2.output, y)
         
         predictions = np.argmax(loss_activation.output, axis = 1 )
